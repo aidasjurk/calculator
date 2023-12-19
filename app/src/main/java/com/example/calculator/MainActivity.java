2d008dece9ab2d5907e88cb11f09c7f9be69df81
aidasjurk <aidasjurk@gmail.com>
2023-12-19 21:39:55 +0200

    Calculatorius is wisho

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,107 +2,93 @@ package com.example.calculator;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.button.MaterialButton;
+import net.objecthunter.exp4j.Expression;
+import net.objecthunter.exp4j.ExpressionBuilder;
 
-    private TextView display;
-    private String currentInput = "";
-    private double currentResult = 0;
-    private String currentOperator = "";
+// ...
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    TextView resultTv, solutionTv;
+    MaterialButton buttonC;
+    MaterialButton buttonDivide, buttonMultiply, buttonPlus, buttonMinus, buttonEquals;
+    MaterialButton button0, button1, button2, button3, button4, button5, button6, button7, button8, button9;
+    MaterialButton buttonAC, buttonDot;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        resultTv = findViewById(R.id.result_tv);
+        solutionTv = findViewById(R.id.solution_tv);
 
-        display = findViewById(R.id.display);
+        assignId(buttonC, R.id.button_c);
+        assignId(buttonDivide, R.id.button_divide);
+        assignId(buttonMultiply, R.id.button_multiply);
+        assignId(buttonPlus, R.id.button_plus);
+        assignId(buttonMinus, R.id.button_minus);
+        assignId(buttonEquals, R.id.button_equals);
+        assignId(button0, R.id.button_0);
+        assignId(button1, R.id.button_1);
+        assignId(button2, R.id.button_2);
+        assignId(button3, R.id.button_3);
+        assignId(button4, R.id.button_4);
+        assignId(button5, R.id.button_5);
+        assignId(button6, R.id.button_6);
+        assignId(button7, R.id.button_7);
+        assignId(button8, R.id.button_8);
+        assignId(button9, R.id.button_9);
+        assignId(buttonAC, R.id.button_ac);
+        assignId(buttonDot, R.id.button_dot);
     }
 
-    public void onButtonClick(View v) {
-        Button button = (Button) v;
+    void assignId(MaterialButton btn, int id) {
+        btn = findViewById(id);
+        btn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        MaterialButton button = (MaterialButton) view;
         String buttonText = button.getText().toString();
+        String dataToCalculate = solutionTv.getText().toString();
 
-        switch (buttonText) {
-            case "+":
-            case "-":
-            case "×":
-            case "÷":
-            case "√":
-                handleOperator(buttonText);
-                break;
-            case "C":
-                clear();
-                break;
-            case "←":
-                deleteLast();
-                break;
-            case "=":
-                calculate();
-                break;
-            default:
-                appendToInput(buttonText);
-                break;
+        if (buttonText.equals("AC")) {
+            solutionTv.setText("");
+            resultTv.setText("0");
+            return;
+        }
+        if (buttonText.equals("=")) {
+            solutionTv.setText(resultTv.getText());
+            return;
+        }
+        if (buttonText.equals("C")) {
+            dataToCalculate = dataToCalculate.substring(0, dataToCalculate.length() - 1);
+        } else {
+            dataToCalculate = dataToCalculate + buttonText;
+        }
+        solutionTv.setText(dataToCalculate);
+
+        String finalResult = getResult(dataToCalculate);
+
+        if (!finalResult.equals("Err")) {
+            resultTv.setText(finalResult);
         }
     }
 
-    private void appendToInput(String text) {
-        currentInput += text;
-        display.setText(currentInput);
-    }
-
-    private void handleOperator(String operator) {
-        if (!currentInput.isEmpty()) {
-            if (!currentOperator.isEmpty()) {
-                calculate();
-            }
-            currentResult = Double.parseDouble(currentInput);
-            currentInput = "";
-            currentOperator = operator;
-        }
-    }
-
-    private void calculate() {
-        if (!currentOperator.isEmpty() && !currentInput.isEmpty()) {
-            double secondOperand = Double.parseDouble(currentInput);
-            switch (currentOperator) {
-                case "+":
-                    currentResult += secondOperand;
-                    break;
-                case "-":
-                    currentResult -= secondOperand;
-                    break;
-                case "×":
-                    currentResult *= secondOperand;
-                    break;
-                case "÷":
-                    if (secondOperand != 0) {
-                        currentResult /= secondOperand;
-                    }
-                    break;
-                case "√":
-                    currentResult = Math.sqrt(currentResult);
-                    break;
-            }
-            currentOperator = "";
-            currentInput = String.valueOf(currentResult);
-            display.setText(currentInput);
-        }
-    }
-
-    private void clear() {
-        currentInput = "";
-        currentResult = 0;
-        currentOperator = "";
-        display.setText("");
-    }
-
-    private void deleteLast() {
-        if (!currentInput.isEmpty()) {
-            currentInput = currentInput.substring(0, currentInput.length() - 1);
-            display.setText(currentInput);
+    String getResult(String data) {
+        try {
+            Expression expression = new ExpressionBuilder(data).build();
+            double result = expression.evaluate();
+            return String.valueOf(result);
+        } catch (Exception e) {
+            return "Err";
         }
     }
 }
+
